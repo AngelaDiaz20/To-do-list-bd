5e54832219953070a602c4098867856f785b13a2
Angela Diaz <angeldi846@gmail.com>
2023-01-26 12:39:35 -0500

    environment variables

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 //Library import that allows an application from another source to access its resources
 const cors = require("cors");
+require('dotenv').config()
 
 //constant to specify the port number on which the server will listen
 const PORT = 3030;
 //Express instance used to configure and handle requests and responses from the server
 const app = express();
+
+const username=process.env.DB_USERNAME;
+const password=process.env.DB_PASSWORD;
+const cluster=process.env.DB_CLUSTER
 
 //route import
 const todoRoutes = require("./routes/todoRoutes");
@@ -26,7 +31,7 @@ app.use(cors());
 
 mongoose
   //database connection
-  .connect("mongodb+srv://Angelus:angelus@grupo2.q6mleqd.mongodb.net/todolist?retryWrites=true&w=majority", connectionOptions)
+  .connect(`mongodb+srv://${username}:${password}@${cluster}.q6mleqd.mongodb.net/todolist?retryWrites=true&w=majority`, connectionOptions)
   //promise result for success or failure connection cases
   .then(() => console.log("Connected successfully"))
   .catch((err) => console.error(err));
